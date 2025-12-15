b91e73b79e7842e89670c4573df56d398d395653
turker12345 <104722406+turker12345@users.noreply.github.com>
2025-12-15 22:01:04 +0300

    statics_and_arrangementadded

--- src/main/java/com/example/oeqaas/OEQAASApplication.java
+++ src/main/java/com/example/oeqaas/OEQAASApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class OEQAASApplication extends Application {
     @Override
     public void start(Stage anaSahne) throws IOException { // "stage" changed to "anaSahne" (Main Stage)
-        FXMLLoader fxmlYukleyici = new FXMLLoader(OEQAASApplication.class.getResource("user_test_selection-view.fxml"));
+        FXMLLoader fxmlYukleyici = new FXMLLoader(OEQAASApplication.class.getResource("statics-view.fxml"));
         Scene sahne = new Scene(fxmlYukleyici.load(), 800, 600); // "scene" changed to "sahne"
         anaSahne.setTitle("Quiz ve Analiz Sistemi");
         // Icon setup
